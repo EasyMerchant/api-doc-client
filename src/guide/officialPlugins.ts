c3c7d509a913b7d7a287eba7d61f3cfc7ba22ca8
jim-easymerchant <92731668+jim-easymerchant@users.noreply.github.com>
2023-10-11 08:32:34 -0500

    Update officialPlugins.ts

--- src/guide/officialPlugins.ts
+++ src/guide/officialPlugins.ts
@@ -6,20 +6,20 @@ export const officialPlugins: Guide = {
   defaultOpen: true,
   content: [
     {
-      id: ‘give-wp’,
-      title: ‘GiveWP,
+      id: 'give-wp',
+      title: 'GiveWP',
       description: [
         {
           paragraph: 'Download and install the {app_name} addon for GiveWP from <a href=“{cdnUrl}/downloads/givewp-em-plugin.zip”>{cdnUrl}/downloads/givewp-em-plugin.zip.</a>',
         },
    {
-      paragraph: ‘Once the {app_name} for GiveWP is activated, go to Donations -> Settings -> Payment Gateways.’,
+      paragraph: 'Once the {app_name} for GiveWP is activated, go to Donations -> Settings -> Payment Gateways.',
   },
         {
           paragraph: 'Click on {app_name}, Enter your Publishable Key, found in your {app_name} Dashboard and Add checkout heading text. here',
           image: {
             src: '{cdnUrl}/assets/givewp-settings.png',
-            alt: ‘GiveWP Settings’,
+            alt: 'GiveWP Settings',
             objectFit: 'cover',
             width: 1000,
             height: 600,
@@ -29,7 +29,7 @@ export const officialPlugins: Guide = {
     paragraph: '{app_name} will be displayed on the donation form as a payment option.',
     image: {
             src: '{cdnUrl}/assets/Donate-Now-givewp.png',
-            alt: ‘Donate Now GiveWP’,
+            alt: 'Donate Now GiveWP',
             objectFit: 'cover',
             width: 1000,
             height: 600,
@@ -39,8 +39,8 @@ export const officialPlugins: Guide = {
 
 
     {
-      id: ‘woocommerce’,
-      title: ‘Woocommerce’,
+      id: 'woocommerce',
+      title: 'Woocommerce',
     },
   },
   ],
