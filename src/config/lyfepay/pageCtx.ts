823d5f1ab6a82d74a776f98da9782a4e173e4047
jim-easymerchant <92731668+jim-easymerchant@users.noreply.github.com>
2023-10-11 15:34:20 -0500

    Update pageCtx.ts

--- src/config/lyfepay/pageCtx.ts
+++ src/config/lyfepay/pageCtx.ts
@@ -1,7 +1,7 @@
 import { IPageContext } from '../interface';
 
 const productionBaseUrl = 'https://api.lyfepay.io/api';
-const sandBoxApiUrl = 'https://sandbox-api.sandbox-covenantpay.io/api';
+const sandBoxApiUrl = 'https://sandbox-api.sandbox-lyfepay.io/api';
 const cdnUrl = 'https://cdn.lyfepay.io';
 
 export const lyfePayPageCtx: IPageContext = {
