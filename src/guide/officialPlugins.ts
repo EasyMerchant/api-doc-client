1b07f06c6269efe9038d5d5d6c4940df9ba8dcf4
jim-easymerchant <92731668+jim-easymerchant@users.noreply.github.com>
2023-10-11 07:25:02 -0500

    Update officialPlugins.ts

--- src/guide/officialPlugins.ts
+++ src/guide/officialPlugins.ts
@@ -14,7 +14,7 @@ description: [
           paragraph: `Download and install the {app_name} addon for GiveWP from <a href=“{cdnUrl}/downloads/givewp-em-plugin.zip”>{cdnUrl}/downloads/givewp-em-plugin.zip.</a> `,
         },
 	 {
-	    paragraph: ‘Once the {app_name} anon for GiveWP is activated, go to Donations -> Settings -> Payment Gateways.’,
+	    paragraph: ‘Once the {app_name} for GiveWP is activated, go to Donations -> Settings -> Payment Gateways.’,
 	},
         {
           paragraph: `Click on {app_name}, Enter your Publishable Key, found in your {app_name} Dashboard and Add checkout heading text. here`,
