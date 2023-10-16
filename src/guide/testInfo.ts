89c98627b1d01ebaf76ef4386cf248cde20491cd
jim-easymerchant <92731668+jim-easymerchant@users.noreply.github.com>
2023-10-16 11:25:03 -0500

    Update testInfo.ts

--- src/guide/testInfo.ts
+++ src/guide/testInfo.ts
@@ -1,1 +1,21 @@
+import { Guide } from './_types';
 
+export const testInfo: Guide = {
+  id: 'testInfo',
+  title: 'Test Info',
+  defaultOpen: true,
+  content: [
+    {
+      id: 'payments',
+      title: 'Payments',
+    },
+    {
+      id: 'onramp-offramp',
+      title: 'Onramp / Offramp',
+    },
+    {
+      id: 'wallets',
+      title: 'Wallets',
+    },
+  ],
+};
