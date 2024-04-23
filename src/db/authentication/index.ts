9600b319a5d0af59140bfbe025bd1b301419dca5
Raja K <raja@easymerchant.io>
2024-04-23 13:41:36 +0530

    brand name fix

--- src/db/authentication/index.ts
+++ src/db/authentication/index.ts
@@ -5,8 +5,8 @@ export const authentication: IApiData = {
   name: 'Authentication',
   defaultOpen: true,
   description: [
-    '{app_name} uses API key/secret pair to allow access to the API. You can get a new EasyMerchant API key by emailing developer support.',
-    `EasyMerchant expects the API key and secret to be included in all API requests to the server in a header that looks like the following: <ul>
+    '{app_name} uses API key/secret pair to allow access to the API. You can get a new {app_name} API key by emailing developer support.',
+    `{app_name} expects the API key and secret to be included in all API requests to the server in a header that looks like the following: <ul>
     <li>
       X-Api-Key: <YOUR_API_KEY>
     </li>
