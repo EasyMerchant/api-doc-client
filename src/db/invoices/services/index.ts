d6ce3df0ea41f0ad5dc3e593d307208be8596394
Manoj Kumar <manoj.k@easymerchant.io>
2024-10-08 12:46:05 +0530

    Invoice transactions update documentation

--- src/db/invoices/services/index.ts
+++ src/db/invoices/services/index.ts
@@ -2,10 +2,12 @@ import { IService } from '~/interfaces';
 import { listAllInvoices } from './listAllInvoices';
 import { createInvoice } from './createInvoice';
 import { sendInvoice } from './sendInvoice';
+import { invoiceTransactions } from './invoiceTransactions';
 
 export const services: IService[] = [
   // list array items serial wise
   listAllInvoices,
   createInvoice,
   sendInvoice,
+  invoiceTransactions
 ];
